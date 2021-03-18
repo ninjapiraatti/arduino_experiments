d2b6997d82b408a26f799bede56bada480e1f670
Ninjapiraatti <tuoppil@gmail.com>
2021-03-18 21:32:05 +0200

    Added tft display to the tank

--- tank/tank.ino
+++ tank/tank.ino
@@ -3,20 +3,35 @@
 #include <Servo.h>
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Fonts/FreeSerif18pt7b.h>
+
+
 // Define Pins
 #define PIN_IN1 10
 #define PIN_IN2 9
 #define PIN_IN3 8
 #define PIN_IN4 7
+
 #define PIN_ST1 6
 #define PIN_ST2 5
 #define PIN_ST3 4
 #define PIN_ST4 3
+
 #define PIN_SRV 2
+
 #define PIN_ENA A0
 #define PIN_ENB A1
+
 #define MAX_THROTTLE 255
 #define MIN_THROTTLE 120
+
+#define TFT_SCK 13
+#define TFT_MOSI 11
+#define TFT_CS 10
+#define TFT_RST 9
+#define TFT_DC 8
 
 char ssid[] = "e33235";        // your network SSID (name)
 char pass[] = "231402922";    // your network password (use for WPA, or use as key for WEP)
@@ -29,9 +44,12 @@ const int GMT = 2; //change this to adapt it to your time zone
 // Define number of steps per rotation:
 const int stepsPerRevolution = 2048;
 
+// Define TFT
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST);
+
 Servo servo;
 Stepper myStepper = Stepper(stepsPerRevolution, PIN_ST1, PIN_ST3, PIN_ST2, PIN_ST4);
-int angle = 10;
+int angle = 0;
 
 void setup() 
 {
@@ -48,6 +66,23 @@ void setup()
   myStepper.setSpeed(15);
   servo.attach(PIN_SRV);
   servo.write(angle);
+
+  tft.initR(INITR_BLACKTAB);  // Initialize a ST7735S chip, black tab
+  tft.fillScreen(ST7735_BLACK);  // Fill screen with black
+  tft.setTextWrap(false);
+  tft.setCursor(0, 0);  // Set position (x,y)
+  tft.setTextColor(ST7735_WHITE);  // Set color of text. First is the color of text and after is color of background
+  tft.setTextSize(1);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
+  tft.println("Hello");  // Print a text or value
+  // Start using a custom font:
+  tft.setFont(&FreeSerif18pt7b);  // Set a custom font
+  tft.setTextSize(0);  // Set text size. We are using custom font so you should always set text size as 0
+  // Write to the display the text "World":
+  tft.setCursor(0, 50);  // Set position (x,y)
+  tft.setTextColor(ST7735_RED);  // Set color of text. We are using custom font so there is no background color supported
+  tft.println("World!");  // Print a text or value
+  // Stop using a custom font:
+  tft.setFont();  // Reset to standard font, to stop using any custom font previously set
 
   // We'll use the serial monitor to view the sensor output
   Serial.begin(9600);
@@ -187,6 +222,7 @@ void printMacAddress(byte mac[]) {
 
 void loop() 
 {
+/*
   Serial.print("Engine loop begin\n");
   for(angle = 10; angle < 180; angle++)  
   {                                  
@@ -204,4 +240,7 @@ void loop()
   stopengines(800);
   turnleft(300);
   stopengines(500);
+  */
+  tft.fillRect(23, 67, 12, 18, ST7735_BLACK);
+  tft.println("Hello");  // Print a text or value
 }
