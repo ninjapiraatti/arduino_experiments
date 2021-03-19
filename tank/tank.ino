870d9e97b0012d0b4afb5879edd67e0ae9f6e47a
Ninjapiraatti <tuoppil@gmail.com>
2021-03-19 22:26:37 +0200

    Moved tft stuff to its own file

--- tank/tank.ino
+++ tank/tank.ino
@@ -9,23 +9,7 @@ void setup()
 	setup_engines();
 	setup_stepper();
 	setup_wifi();
-	tft.initR(INITR_BLACKTAB);  // Initialize a ST7735S chip, black tab
-	tft.fillScreen(ST7735_BLACK);  // Fill screen with black
-	tft.setTextWrap(false);
-	tft.setRotation(1);
-	tft.setCursor(0, 0);  // Set position (x,y)
-	tft.setTextColor(ST7735_WHITE);  // Set color of text. First is the color of text and after is color of background
-	tft.setTextSize(1);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
-	tft.println("Hello");  // Print a text or value
-	// Start using a custom font:
-	tft.setTextSize(0);  // Set text size. We are using custom font so you should always set text size as 0
-	// Write to the display the text "World":
-	tft.setCursor(0, 50);  // Set position (x,y)
-	tft.setTextColor(ST7735_RED);  // Set color of text. We are using custom font so there is no background color supported
-	tft.println("World!");  // Print a text or value
-	// Stop using a custom font:
-	tft.setFont();  // Reset to standard font, to stop using any custom font previously set
-	// We'll use the serial monitor to view the sensor output
+	setup_tft(tft);
 	Serial.begin(9600);
 }
 
---
+++ tank/tft.c
@@ -0,0 +1,16 @@
+#include "tank.h"
+
+void setup_tft(Adafruit_ST7735 tft) {
+	tft.initR(INITR_BLACKTAB);  // Initialize a ST7735S chip, black tab
+	tft.fillScreen(ST7735_BLACK);  // Fill screen with black
+	tft.setTextWrap(false);
+	tft.setRotation(1);
+	tft.setCursor(0, 0);  // Set position (x,y)
+	tft.setTextColor(ST7735_WHITE);  // Set color of text. First is the color of text and after is color of background
+	tft.setTextSize(1);  // Set text size. Goes from 0 (the smallest) to 20 (very big)
+	tft.println("Hello");  // Print a text or value
+	tft.setTextSize(0);  // Set text size. We are using custom font so you should always set text size as 0
+	tft.setCursor(0, 50);  // Set position (x,y)
+	tft.setTextColor(ST7735_RED);  // Set color of text. We are using custom font so there is no background color supported
+	tft.println("World!");  // Print a text or value
+}
