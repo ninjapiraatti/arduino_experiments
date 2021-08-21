509b7405f09e87e8d46338d52fc8ba61a359b683
Ninjapiraatti <tuoppil@gmail.com>
2021-08-21 08:47:47 +0300

    Removed secrets from remote

--- tank/arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-char SSID[] = "4G-Gateway-EF11";        // your network SSID (name)
-char PASS[] = "M6EQEH92A7G";
-
-char TANKKI_URL[] = "hairdresser.cloudmqtt.com";
-int TANKKI_PORT = 16995;
-char TANKKI_USER[] = "sdclgvuj";
-char TANKKI_PASS[] = "3fXiuMjbQXyd";
-
-
